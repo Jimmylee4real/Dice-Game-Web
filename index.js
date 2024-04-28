8427c9b578bbd849dbe0cee9d2bb36b03042e892
Jimmy Lee <59492829+Jimmylee4real@users.noreply.github.com>
2024-04-28 12:45:34 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+/* draw random image for left dice */
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+document.querySelectorAll("img")[0].setAttribute('src', randomImageSource);
+
+/* draw random image for right dice */
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage02 = "images/dice" + randomNumber2 + ".png"
+
+document.querySelectorAll("img")[1].setAttribute('src', randomDiceImage02);
+
+/* change the text in h1 */
+if (randomNumber1 === randomNumber2) {
+  document.querySelector('h1').innerHTML = "It's a DRAW! We''ll see what happens next game😉";
+} 
+else if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = "the WINNER goes to Player 1!😜";
+}
+else {
+  document.querySelector('h1').innerHTML = "the WINNER goes to Player 2!😜";
+}
